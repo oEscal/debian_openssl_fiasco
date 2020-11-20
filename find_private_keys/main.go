5b9cdb222c074f9cc801abdc4f37fd7559dec99c
escaleira <pedroescaleira@hotmail.com>
2020-11-20 11:59:50 +0000

    calculating gcd

--- find_private_keys/main.go
+++ find_private_keys/main.go
@@ -5,9 +5,12 @@ import (
 	"os"
 	"bufio"
 	"log"
+	"strings"
+	"math/big"
 )
 
 func main() {
+	publicModulusMap := make(map[string]string)
 
 	// read file
 	file, err := os.Open("rsa_public_info.txt")
@@ -19,7 +22,8 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		println(scanner.Text())
+		line := strings.Split(scanner.Text(), "\t")
+		publicModulusMap[line[0]] = line[1]
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -27,5 +31,22 @@ func main() {
 	}
 
 
-
+	bigOne := big.NewInt(1)
+	gcd := big.NewInt(1)
+	for person1 := range publicModulusMap {
+		var num, _ = new(big.Int).SetString(publicModulusMap[person1], 10)
+		
+		for person2 := range(publicModulusMap) {
+			go func(person1, person2 string) {
+				if person1 != person2 {
+					var num2, _ = new(big.Int).SetString(publicModulusMap[person2], 10)
+					
+					gcd.GCD(nil, nil, num, num2)
+					if gcd.Cmp(bigOne) != 0 {
+						println(person1, person2, gcd.Text(10))
+					}
+				}
+			} (person1, person2)
+		}
+	}
 }

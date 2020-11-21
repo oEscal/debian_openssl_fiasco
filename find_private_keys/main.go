ed73a193a2446e339959acde72b8d2c11b9522e4
escaleira <pedroescaleira@hotmail.com>
2020-11-21 19:26:29 +0000

    some more fixes on find private keys main

--- find_private_keys/main.go
+++ find_private_keys/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"bufio"
 	"log"
@@ -9,8 +10,11 @@ import (
 )
 
 
+type void struct{}
+
+
 func main() {
-	publicModulusMap := make(map[string]string)
+	publicModulusMap := make(map[string][]string)
 
 	// read file
 	file, err := os.Open("rsa_public_info.txt")
@@ -23,7 +27,9 @@ func main() {
 
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), "\t")
-		publicModulusMap[line[0]] = line[1]
+		publicModulusMap[line[0]] = make([]string, 2)
+		publicModulusMap[line[0]][0] = line[1]
+		publicModulusMap[line[0]][1] = line[2]
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -32,26 +38,35 @@ func main() {
 
 
 	bigOne := big.NewInt(1)
-	gcd := big.NewInt(1)
 	for person1 := range publicModulusMap {
-		var num, _ = new(big.Int).SetString(publicModulusMap[person1], 10)
+		publicInfoPerson1 := publicModulusMap[person1]
+		var privateKeyPerson1, _ = new(big.Int).SetString(publicInfoPerson1[0], 10)
 
 		flag := false
 
 		for person2 := range(publicModulusMap) {
-			go func(person1, person2 string) {
-				if person1 == person2 {
-					flag = true
-				}
-				if person1 != person2 && flag {
-					var num2, _ = new(big.Int).SetString(publicModulusMap[person2], 10)
+			if flag {
+				go func(person1, person2 string) {
+					q := big.NewInt(1)
+					p := big.NewInt(1)
 					
-					gcd.GCD(nil, nil, num, num2)
-					if gcd.Cmp(bigOne) != 0 {
-						println(person1, person2, gcd.Text(10))
-					}
-				}
-			} (person1, person2)
+					publicInfoPerson2 := publicModulusMap[person2]
+					var privateKeyPerson2, _ = new(big.Int).SetString(publicInfoPerson2[0], 10)
+					
+					q.GCD(nil, nil, privateKeyPerson1, privateKeyPerson2)
+					if q.Cmp(bigOne) != 0 {
+						// person, q, p, m, public exponent
+						fmt.Printf("%s\t%s\t%s\t%s\t%s\n", 
+							person1, q.Text(10), p.Quo(privateKeyPerson1, q).Text(10), publicInfoPerson1[0], publicInfoPerson1[1])
+						fmt.Printf("%s\t%s\t%s\t%s\t%s\n", 
+							person2, q.Text(10), p.Quo(privateKeyPerson2, q).Text(10), publicInfoPerson2[0], publicInfoPerson2[1])
+					} 
+				} (person1, person2)
+			}
+
+			if person1 == person2 {
+				flag = true
+			}
 		}
 	}
 }
